b3ce2bccf8ce0c29b77c35f669223482a971c608
Alexandre Magno de S Thiago Filho <alexandremstf@gmail.com>
2020-04-07 11:16:59 -0300

    Change default values (PID and Feedrate)

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -484,9 +484,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 23.36
-  #define DEFAULT_Ki 2.23
-  #define DEFAULT_Kd 61.12
+  #define DEFAULT_Kp 16.44
+  #define DEFAULT_Ki 1.18
+  #define DEFAULT_Kd 57.24
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -535,9 +535,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 247.72
-  #define DEFAULT_bedKi 48.77
-  #define DEFAULT_bedKd 314.54
+  #define DEFAULT_bedKp 118.22
+  #define DEFAULT_bedKi 23.28
+  #define DEFAULT_bedKd 400.29
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
@@ -730,14 +730,14 @@
  * Override with M92
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 150 }
+#define DEFAULT_AXIS_STEPS_PER_UNIT   { 100, 100, 1600, 300 }
 
 /**
  * Default Max Feed Rate (mm/s)
  * Override with M203
  *                                      X, Y, Z, E0 [, E1[, E2...]]
  */
-#define DEFAULT_MAX_FEEDRATE          { 200, 200, 10, 50 }
+#define DEFAULT_MAX_FEEDRATE          { 200, 200, 5, 50 }
 
 //#define LIMITED_MAX_FR_EDITING        // Limit edit via M203 or LCD to DEFAULT_MAX_FEEDRATE * 2
 #if ENABLED(LIMITED_MAX_FR_EDITING)
