90d7f33fd21c2fc357e7934956fe3acdb82216ea
Alexandre Magno de S Thiago Filho <alexandremstf@gmail.com>
2020-04-07 12:41:04 -0300

    Update PID values

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -493,9 +493,9 @@
   // If you are using a pre-configured hotend then you can use one of the value sets by uncommenting it
 
   // Ultimaker
-  #define DEFAULT_Kp 22.2
-  #define DEFAULT_Ki 1.08
-  #define DEFAULT_Kd 114
+  #define DEFAULT_Kp 27.07
+  #define DEFAULT_Ki 3.41
+  #define DEFAULT_Kd 53.77
 
   // MakerGear
   //#define DEFAULT_Kp 7.0
@@ -544,9 +544,9 @@
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from FOPDT model - kp=.39 Tp=405 Tdead=66, Tc set to 79.2, aggressive factor of .15 (vs .1, 1, 10)
-  #define DEFAULT_bedKp 10.00
-  #define DEFAULT_bedKi .023
-  #define DEFAULT_bedKd 305.4
+  #define DEFAULT_bedKp 51.54
+  #define DEFAULT_bedKi 10.15
+  #define DEFAULT_bedKd 174.5
 
   //120V 250W silicone heater into 4mm borosilicate (MendelMax 1.5+)
   //from pidautotune
